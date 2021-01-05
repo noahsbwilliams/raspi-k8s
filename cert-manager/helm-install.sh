207eaa020bbcafab1ad714efc20dd86abfefcb4d
Noah Williams <public@noahsbwilliams.com>
2021-01-05 12:23:17 -0900

    Add cheeky commentary

--- cert-manager/helm-install.sh
+++ cert-manager/helm-install.sh
@@ -1,5 +1,6 @@
 kubectl create namespace cert-manager;
 
+# This script doesn't work because I haven't bothered to escape the semicolons, just run these one at a time...
 helm repo add jetstack https://charts.jetstack.io ;
 
 helm repo update;
